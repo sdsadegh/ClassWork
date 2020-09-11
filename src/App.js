d7094ec984d58a9792afbc1e7bdb290fb0fa7877
sdsadegh <engineerhashemi.iran@gmail.com>
2020-09-11 10:48:44 -0700

    Initi

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import logo from './logo.svg';
+import Blog from './Containers/Blog/Blog';
+import './App.css';
+
+
+function App() {
+  return (
+    <div>
+      <Blog/>
+    </div>
+  );
+}
+
+export default App;
